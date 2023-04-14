db85f698085b88a76d1c7c19e610602240747538
rahulktd <rahulkpktd@gmail.com>
2023-04-14 09:31:01 +0530

    Made some changes in templates. And added a date adding widget.

--- ToDoApp/forms.py
+++ ToDoApp/forms.py
@@ -3,8 +3,12 @@ from django import forms
 from ToDoApp.models import ToDo
 
 
+class Dateinput(forms.DateInput):
+    input_type = 'date'
+
 class ToDoForm(forms.ModelForm):
+    date = forms.DateField(widget=Dateinput)
     class Meta:
         model = ToDo
-        fields = ("__all__")
+        fields = ('event','date')
 
